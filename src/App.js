a011a59d81aebee34f62a9e3bc518e3e8ae28667
HieuNguyen-1074 <hjeu1074@gmail.com>
2021-05-28 22:18:52 +0700

    ss

--- src/App.js
+++ src/App.js
@@ -22,21 +22,22 @@ function App() {
   return (
     <div className="photo-app">
       <Suspense fallback={<div>loadding ...</div>}>
-        <BrowserRouter>
-          <Header />
-          <ul>
-            <li><Link to='/photo'> Go to photo page</Link></li>
-            <li> <Link to='photo/add'>Go to add new photo page</Link></li>
-            <li><Link to='photo/123'>Go to edit photo page</Link></li>
-          </ul>
-          <Switch>
+        {/* <BrowserRouter> */}
+        <Header />
+        <ul>
+          <li><Link to='/photo'> Go to photo page</Link></li>
+          <li> <Link to='photo/add'>Go to add new photo page</Link></li>
+          <li><Link to='photo/123'>Go to edit photo page</Link></li>
+          <Photo />
+        </ul>
+        {/* <Switch>
             {/* <Redirect exact from='/' to='/photo' /> */}
 
-            <Route path='/' component={Photo} />
+        {/* <Route path='/' component={Photo} />
             <Route component={NotFound} />
 
-          </Switch>
-        </BrowserRouter>
+          </Switch>  */}
+        {/* </BrowserRouter> */}
       </Suspense>
     </div>
   );
